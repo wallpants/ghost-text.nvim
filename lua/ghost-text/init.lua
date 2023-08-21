b002b9c1d42a1aa06c4e10199bd98c41d921ca51
Gual Casas <gual.casas@gmail.com>
2023-08-21 13:42:26 -0500

    create utils file

--- lua/ghost-text/init.lua
+++ lua/ghost-text/init.lua
@@ -1,31 +1,11 @@
+local Utils = require("ghost-text.utils")
+
 local M = {}
-
-local current_file_path = debug.getinfo(1, "S").source:sub(2)
-local plugin_root = vim.fn.fnamemodify(current_file_path, ":p:h:h:h") .. "/"
-local serverlist = vim.fn.serverlist()
-local nvim_socket = serverlist[1]
-
----@param log_output string
----@param source string
-local log = function(log_output, source)
-	return function(_, data)
-		if log_output == "print" then
-			print(source .. vim.inspect(data))
-		end
-		if log_output == "file" then
-			local file = io.open(plugin_root .. source .. ".log", "a")
-			if file then
-				file:write(vim.inspect(data) .. "\n")
-				file:close()
-			end
-		end
-	end
-end
 
 ---@class opts
 ---@field autostart boolean
 ---@field port number
----@field log_output string -- "none" | "file" | "print"
+---@field log_output string -- "none" | "file" | "print"
 --
 ---@param opts opts
 M.setup = function(opts)
@@ -40,7 +20,7 @@ M.setup = function(opts)
 	-- set vars to be read and used by node-client
 	vim.g.ghost_text_port = opts.port
 
-	local shell_command = "node " .. plugin_root .. "dist/index.js " .. nvim_socket
+	local shell_command = "node " .. Utils.plugin_root .. "dist/index.js " .. Utils.nvim_socket
 
 	local function start_server()
 		vim.api.nvim_create_autocmd({ "TextChangedI", "TextChanged" }, {
@@ -58,9 +38,9 @@ M.setup = function(opts)
 		})
 
 		vim.fn.jobstart(shell_command, {
-			on_stdout = log(opts.log_output, "stdout"),
-			on_stderr = log(opts.log_output, "stderr"),
-			on_exit = log(opts.log_output, "exit"),
+			on_stdout = Utils.log(opts.log_output, "stdout"),
+			on_stderr = Utils.log(opts.log_output, "stderr"),
+			on_exit = Utils.log(opts.log_output, "exit"),
 		})
 	end
 
