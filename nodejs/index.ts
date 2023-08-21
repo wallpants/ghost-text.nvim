db2f2d4e2487967f3220025f8fbfea5506542bd1
Gual Casas <gual.casas@gmail.com>
2023-08-20 23:33:29 -0500

    move isServerRunning into main & read port from argvs

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,12 +1,20 @@
 import { attach } from "neovim";
-import { isServerRunning } from "./is-running";
 import { startServer } from "./server";
 
-const PORT = 4001;
 const socket = process.argv[2];
+const PORT = Number(process.argv[3]);
+
+async function isServerRunning() {
+    try {
+        await fetch(`http://localhost:${PORT}`);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
 
 async function main() {
-    if (await isServerRunning(PORT)) return;
+    if (await isServerRunning()) return;
     const nvim = attach({ socket });
     await startServer(nvim, PORT);
 }
